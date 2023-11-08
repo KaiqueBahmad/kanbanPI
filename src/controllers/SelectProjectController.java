5efe6b1ca891486a118edb51192ea42165778e57
Lisa <82837765+Isaluh@users.noreply.github.com>
2023-11-08 17:50:23 -0300

    Add files via upload
    telas funfando

---
+++ src/controllers/SelectProjectController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SelectProjectController {
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Button novoProjeto;
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void novoProjeto(ActionEvent event) {
+
+    }
+
+}
