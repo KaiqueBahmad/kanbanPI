af3d1ba3de9c9804331053bf208624f370c73d72
Lisa <82837765+Isaluh@users.noreply.github.com>
2023-11-09 18:19:46 -0300

    Add files via upload

---
+++ kanbanPI/src/controllers/CreateProjectController.java
@@ -0,0 +1,71 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreateProjectController {
+
+    @FXML
+    private Button cancelarProjeto;
+
+    @FXML
+    private Button criarProjeto;
+
+    @FXML
+    private TextArea descricaoNovoProjeto;
+
+    @FXML
+    private TextField nomeNovoProjeto;
+
+    @FXML
+    private Label nomeProjetoDois;
+
+    @FXML
+    private Label nomeProjetoQuatro;
+
+    @FXML
+    private Label nomeProjetoTres;
+
+    @FXML
+    private Label nomeProjetoUm;
+
+    @FXML
+    private Label numPostProjetoDois;
+
+    @FXML
+    private Label numPostProjetoQuatro;
+
+    @FXML
+    private Label numPostProjetoTres;
+
+    @FXML
+    private Label numPostProjetoUm;
+
+    @FXML
+    private ProgressIndicator progressoTotalDois;
+
+    @FXML
+    private ProgressIndicator progressoTotalQuatro;
+
+    @FXML
+    private ProgressIndicator progressoTotalTres;
+
+    @FXML
+    private ProgressIndicator progressoTotalUm;
+
+    @FXML
+    void cancelarProjeto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void criarProjeto(ActionEvent event) {
+
+    }
+
+}
